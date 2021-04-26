6761e997d117c6201c19548bb2cd10135f681a21
TheoFuCoronel <83075750+TheoFuCoronel@users.noreply.github.com>
2021-04-26 10:20:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
+function Start_Round () {
+    Button_A_Value = 0
+    Button_B_Value = 0
+    Added_number = randint(1, 5)
+    Added_number_2 = randint(1, 5)
+    Player_1_points = 0
+    Player_2_points = 0
+}
 input.onButtonPressed(Button.A, function () {
     if (Button_A_Value == 0) {
         Player_1_number = randint(1, 5)
-        if (Player_1_number == 1) {
-            basic.showNumber(1)
-        } else if (Player_1_number == 2) {
-            basic.showNumber(2)
-        } else if (Player_1_number == 3) {
-            basic.showNumber(3)
-        } else if (Player_1_number == 4) {
-            basic.showNumber(4)
-        } else if (Player_1_number == 5) {
-            basic.showNumber(5)
+        if (Player_1_number < 11) {
+            basic.showNumber(Player_1_number)
         }
         basic.showLeds(`
             . . . . .
@@ -21,28 +21,8 @@ input.onButtonPressed(Button.A, function () {
             `)
     } else {
         Player_1_number = Added_number + Player_1_number
-        if (Player_1_number == 1) {
-            basic.showNumber(1)
-        } else if (Player_1_number == 2) {
-            basic.showNumber(2)
-        } else if (Player_1_number == 3) {
-            basic.showNumber(3)
-        } else if (Player_1_number == 4) {
-            basic.showNumber(4)
-        } else if (Player_1_number == 5) {
-            basic.showNumber(5)
-        } else if (Player_1_number == 6) {
-            basic.showNumber(6)
-        } else if (Player_1_number == 7) {
-            basic.showNumber(7)
-        } else if (Player_1_number == 8) {
-            basic.showNumber(8)
-        } else if (Player_1_number == 9) {
-            basic.showNumber(9)
-        } else if (Player_1_number == 11) {
-            basic.showNumber(11)
-        } else if (Player_1_number == 10) {
-            basic.showNumber(10)
+        if (Player_1_number < 11) {
+            basic.showNumber(Player_1_number)
         }
         Added_number = randint(1, 5)
     }
@@ -54,16 +34,8 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     if (Button_B_Value == 0) {
         Player_2_Number = randint(1, 5)
-        if (Player_2_Number == 1) {
-            basic.showNumber(1)
-        } else if (Player_2_Number == 2) {
-            basic.showNumber(2)
-        } else if (Player_2_Number == 3) {
-            basic.showNumber(3)
-        } else if (Player_2_Number == 4) {
-            basic.showNumber(4)
-        } else if (Player_2_Number == 5) {
-            basic.showNumber(5)
+        if (Player_2_Number < 11) {
+            basic.showNumber(Player_2_Number)
         }
         basic.showLeds(`
             . . . . .
@@ -74,28 +46,8 @@ input.onButtonPressed(Button.B, function () {
             `)
     } else {
         Player_2_Number = Added_number_2 + Player_2_Number
-        if (Player_2_Number == 1) {
-            basic.showNumber(1)
-        } else if (Player_2_Number == 2) {
-            basic.showNumber(2)
-        } else if (Player_2_Number == 3) {
-            basic.showNumber(3)
-        } else if (Player_2_Number == 4) {
-            basic.showNumber(4)
-        } else if (Player_2_Number == 5) {
-            basic.showNumber(5)
-        } else if (Player_2_Number == 6) {
-            basic.showNumber(6)
-        } else if (Player_2_Number == 7) {
-            basic.showNumber(7)
-        } else if (Player_2_Number == 8) {
-            basic.showNumber(8)
-        } else if (Player_2_Number == 9) {
-            basic.showNumber(9)
-        } else if (Player_2_Number == 11) {
-            basic.showNumber(11)
-        } else if (Player_2_Number == 10) {
-            basic.showNumber(10)
+        if (Player_2_Number < 11) {
+            basic.showNumber(Player_2_Number)
         }
         Added_number_2 = randint(1, 5)
     }
@@ -142,12 +94,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     } else {
         basic.showString("Tie")
     }
-    Player_2_Number = 0
-    Player_1_number = 0
-    Added_number_2 = 0
-    Added_number = 0
-    Button_B_Value = 0
-    Button_A_Value = 0
+    Start_Round()
 })
 let Player_2_Number = 0
 let Player_1_number = 0
@@ -157,12 +104,7 @@ let Added_number_2 = 0
 let Added_number = 0
 let Button_B_Value = 0
 let Button_A_Value = 0
-Button_A_Value = 0
-Button_B_Value = 0
-Added_number = randint(1, 5)
-Added_number_2 = randint(1, 5)
-Player_1_points = 0
-Player_2_points = 0
+Start_Round()
 basic.forever(function () {
     if (Player_1_points == 3) {
         basic.showString("P1 Wins")
